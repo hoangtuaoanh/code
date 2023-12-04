ad498c85903889c57b02827ec9194773f9f25bcc
HOANGHUAN <70214657+hoangtuaoanh@users.noreply.github.com>
2023-12-04 15:43:47 +0700

    Update Duplication_Remote.py

--- Duplication_Remote.py
+++ Duplication_Remote.py
@@ -1,11 +1,19 @@
-def master():
+def cal():
     from openpyxl import load_workbook
     wi = load_workbook(filename= 'Route Master.xlsx')
     sheet_ranges = wi['route']
     maxi = sheet_ranges.max_row
-    setM = {}
-    f = open("c.txt","a")
+    List1 = []
     for i in range(3 , maxi+1):
-        print(setM.add(sheet_ranges[f"E{i}"].value))
-
-master()
+        List1.append(sheet_ranges[f"E{i}"].value)
+    pass
+    group = set(List1)
+    we = load_workbook(filename= 'End_Cal.xlsx')
+    L = we['End_Cal']
+    i = 3
+    for x in group:
+        L[f"E{i}"].value = x
+        #print(L[f"E{i}"].value)
+        i+=1
+    we.save('End_Cal.xlsx')
+cal()
